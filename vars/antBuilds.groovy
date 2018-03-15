47680d3d7b29847f58bc9153ea465d64ad53be1b
Jorge Viegas <jorge.viegas01@sap.com>
2018-03-15 18:39:12 -0300

    Update Jenkinsfile

--- vars/antBuilds.groovy
+++ vars/antBuilds.groovy
@@ -1,8 +1,13 @@
-def call(text, channel) {
-    def slackURL = 'https://hooks.slack.com/services/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'
-    def payload = JsonOutput.toJson([text      : text,
-                                     channel   : channel,
-                                     username  : "jenkins",
-                                     icon_emoji: ":jenkins:"])
-    sh "curl -X POST --data-urlencode \'payload=${payload}\' ${slackURL}"
+#!/usr/bin/env groovy
+
+def call() {
+
+    dir (env.PLATFORM_HOME) {
+     	sh '. ./setantenv.sh'
+     	sh 'ant clean'
+    }
+
+    dir (env.HYBRIS_HOME) {
+     	sh 'ant envconfig'
+    }
 }
