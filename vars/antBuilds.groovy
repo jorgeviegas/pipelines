388a9c056b7c145cb6f076cf40a238d339edfbde
Jorge Viegas <jorge.viegas01@sap.com>
2018-03-16 11:50:14 -0300

    Update Jenkinsfile

--- vars/antBuilds.groovy
+++ vars/antBuilds.groovy
@@ -7,9 +7,12 @@ def call(String platform = '', String hybrisHome = '') {
     		sh 'chmod +x /Users/i848340/.jenkins/workspace/pipelines_pipelines_master-W7SQZS5ABY7IAIKK4C2KSUSI5GLX36NC3O2BK4J3KEMIYCXEJSFQ/hybris/bin/platform/apache-ant-1.9.1/bin/ant'
      		sh 'ant clean'
     	}
-	}
 
-    dir (hybrisHome) {
-     	sh 'ant envconfig'
-    }
+    	 dir (hybrisHome) {
+     		sh 'ant envconfig'
+     		sh 'ant customize'
+			sh 'ant reinstal_addons'
+     		sh 'ant all'
+   		}
+	}  
 }
