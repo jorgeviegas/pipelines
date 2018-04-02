6cee22bc6376b4f7d60c84d086f42e5714654274
Jorge Viegas <jorge.viegas01@sap.com>
2018-04-02 13:40:34 -0300

    Update Jenkinsfile

---
+++ vars/deployToConsolidation.groovy
@@ -0,0 +1,34 @@
+#!/usr/bin/env groovy
+
+def call() {
+
+    // Stop hybris server
+    dir (env.YCONS_PLATFORM_HOME){
+        sh './hybrisserver.sh stop'
+    }
+    
+    // Remove old version platform binaries
+    dir ("${env.YCONS_PATH}/hybris"){
+        sh 'rm -rf bin config'
+    }
+    
+    // Replace platform binaries with new version
+    dir (env.HYBRIS_HOME){
+        unzip zipFile:"temp/hybris/hybrisServer/hybrisServer-AllExtensions.zip", dir:"${env.YCONS_PATH}", quiet:true
+        unzip zipFile:"temp/hybris/hybrisServer/hybrisServer-Platform.zip", dir:"${env.YCONS_PATH}", quiet:true
+        unzip zipFile:"temp/hybris/hybrisServer/hybrisServer-Config.zip", dir:"${env.YCONS_PATH}", quiet:true        
+        unzip zipFile:"temp/hybris/hybrisServer/hybrisServer-Licence.zip", dir:"${env.YCONS_PATH}", quiet:true        
+    }
+    
+    // Copy tomcat configuration and validate config folder
+    withAnt('hybris-cons-ant'){
+         dir(env.YCONS_PLATFORM_HOME){
+            sh 'ant'
+         }
+    }
+    
+    // Start server with new version
+    dir (env.YCONS_PLATFORM_HOME){
+        sh './hybrisserver.sh start'
+    }
+}
