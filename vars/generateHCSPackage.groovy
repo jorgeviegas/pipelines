6e0dbfaa4e466220ccca7a3a7a123e1676e71322
Jorge Viegas <jorge.viegas01@sap.com>
2018-04-09 19:00:51 -0300

    Update Jenkinsfile

--- vars/generateHCSPackage.groovy
+++ vars/generateHCSPackage.groovy
@@ -3,26 +3,32 @@
 def call(Map conf = [:], String env) {
 
     dir (conf.hybris_home) {
-     
-        // Clear temp hcs packages
-        sh 'rm -rf temp_hcs_package'
-        sh 'mkdir temp_hcs_package'
-
+        // Clear old hcs packages
+        sh 'rm -rf hcs_package_skeleton'
     }
     
-    unzip zipFile:conf.hcs_package_skeleton, dir:"${conf.hybris_home}/temp_hcs_package", quiet:true
+    unzip zipFile:conf.hcs_package_skeleton, dir:conf.hybris_home, quiet:true
    
     dir (conf.hybris_home) {
 
-        sh "cp temp/hybris/hybrisServer/hybrisServer-AllExtensions.zip temp_hcs_package/hcs_package_skeleton/hybris/bin"
-        sh "cp temp/hybris/hybrisServer/hybrisServer-Platform.zip temp_hcs_package/hcs_package_skeleton/hybris/bin"
+        sh "cp temp/hybris/hybrisServer/hybrisServer-AllExtensions.zip hcs_package_skeleton/hybris/bin"
+        sh "cp temp/hybris/hybrisServer/hybrisServer-Platform.zip hcs_package_skeleton/hybris/bin"
         
-        sh "cp config/local.properties temp_hcs_package/hcs_package_skeleton/hybris/config/stag/customer.properties"
+        sh "cp config/local.properties hcs_package_skeleton/hybris/config/stag/customer.properties"
 
         def packageName = "${conf.hcs_customer_id}-${conf.hcs_project_id}_${conf.hcs_package_version}"
         echo packageName
 
-        // ssh
+
+        zip zipFile:packageName dir:"hcs_package_skeleton"
+
+        // ssh pack to hcs
+
+        // move package to archive
+        sh "mv ${packageName} ${conf.hcs_packages_folder}"
+
+        // Clear the workspace
+        sh 'rm -rf hcs_package_skeleton'
 
     }
 }
