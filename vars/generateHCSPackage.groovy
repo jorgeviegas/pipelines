31e9abec57eff4b8065ce9a49edddc69481849b7
Jorge Viegas <jorge.viegas01@sap.com>
2018-04-09 18:33:54 -0300

    Update Jenkinsfile

--- vars/generateHCSPackage.groovy
+++ vars/generateHCSPackage.groovy
@@ -2,13 +2,17 @@
 
 def call(Map conf = [:], String env) {
 
-   dir (conf.hybris_home) {
+    dir (conf.hybris_home) {
      
         // Clear temp hcs packages
         sh 'rm -rf temp_hcs_package'
         sh 'mkdir temp_hcs_package'
 
-        unzip zipFile:conf.hcs_package_skeleton, dir:'temp_hcs_package', quiet:true
+    }
+    
+    unzip zipFile:conf.hcs_package_skeleton, dir:"${conf.hybris_home}/temp_hcs_package", quiet:true
+   
+    dir (conf.hybris_home) {
 
         sh "cp temp/hybris/hybrisServer/hybrisServer-AllExtensions.zip temp_hcs_package/hcs_package_skeleton/hybris/bin"
         sh "cp temp/hybris/hybrisServer/hybrisServer-Platform.zip temp_hcs_package/hcs_package_skeleton/hybris/bin"
