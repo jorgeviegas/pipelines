9aaeb937c3f99668eeb5641db54e013cf7527685
i863273 <r.berlezi@sap.com>
2018-04-13 17:35:41 -0300

    path change

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -4,7 +4,7 @@ def call(Map conf = [:]) {
 
    // withSonarQubeEnv('chedraui-sonar-qube'){
         def scannerHome = tool 'chedraui-sonar-scanner';
-        dir (workspace) {
+        dir (workspace + conf.hybris_home) {
             sh "${scannerHome}bin/sonar-scanner -Dproject.settings=$workspace" + conf.sonar_configuration_file
         }
 	// }
