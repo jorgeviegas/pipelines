df420cec0fe9793b6dc5c1578517b293d0d86c35
i863273 <r.berlezi@sap.com>
2018-04-13 16:05:27 -0300

    removed echo tests

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -3,11 +3,8 @@
 def call(Map conf = [:]) {
 
    // withSonarQubeEnv('chedraui-sonar-qube'){
-    	echo "test test test"
         def scannerHome = tool 'chedraui-sonar-scanner';
         dir (conf.sonar_configuration_path) {
-        	echo "test test test"
-        	echo conf.sonar_configuration_path
             sh "${scannerHome}/bin/sonar-scanner"
         }
 	// }
