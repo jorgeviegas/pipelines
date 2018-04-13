63c15021713703ef296e50c60abf88e1e350f884
i863273 <r.berlezi@sap.com>
2018-04-13 17:06:36 -0300

    scanner test

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -5,7 +5,7 @@ def call(Map conf = [:]) {
    // withSonarQubeEnv('chedraui-sonar-qube'){
    		echo "${workspace}"
         def scannerHome = tool 'chedraui-sonar-scanner';
-        dir (workspace) {
+        dir (workspace + conf.sonar_configuration_path) {
             sh "${scannerHome}/bin/sonar-scanner"
         }
 	// }
