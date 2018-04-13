dd073b9434f97ab68721cfe45fe7e23486f8fa7d
i863273 <r.berlezi@sap.com>
2018-04-13 15:44:15 -0300

    fixe prop name

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -5,9 +5,9 @@ def call(Map conf = [:]) {
    // withSonarQubeEnv('chedraui-sonar-qube'){
     	echo "test test test"
         def scannerHome = tool 'chedraui-sonar-scanner';
-        dir (conf.sonar_configuration_file) {
+        dir (conf.sonar_configuration_path) {
         	echo "test test test"
-        	echo conf.sonar_configuration_file
+        	echo conf.sonar_configuration_path
             sh "${scannerHome}/bin/sonar-scanner"
         }
 	// }
