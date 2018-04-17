e75434a9d6bcedd3e810f7f07bdb778b4126d6d3
Cassiano Jaeger Stradolini <cassianojaeger@gmail.com>
2018-04-17 11:15:45 -0300

    scaping quotes and testing sh to translate the code

--- vars/buildFortify.groovy
+++ vars/buildFortify.groovy
@@ -4,10 +4,10 @@ def call(Map conf = [:]) {
 
   dir(conf.fortify_binaries){
 
-    echo './sourceanalyzer -b conf.fortify.buildID -cp ${workspace}conf.fortify_javaFiles -exclude \\"${workspace}${conf.fortify_excludeTestGenSrc}\\"'
+    echo "./sourceanalyzer -b conf.fortify.buildID -cp ${workspace}conf.fortify_javaFiles -exclude \"${workspace}${conf.fortify_excludeTestGenSrc}\""
 
-    // sh './sourceanalyzer -b conf.fortify.buildID -cp ${workspace}conf.fortify.javaFiles -exclude "${workspace}${conf.fortify.excludeTestGenSrc}"'
-    //sh '${conf.fortify_binaries}/sourceanalyzer -b conf.fortify.buildID -cp conf.fortify.jsFiles -exclude conf.fortify.excludeGeneratedJSFiles'
+    sh "./sourceanalyzer -b conf.fortify.buildID -cp ${workspace}conf.fortify.javaFiles -exclude \"${workspace}${conf.fortify.excludeTestGenSrc}\""
+    // sh '${conf.fortify_binaries}/sourceanalyzer -b conf.fortify.buildID -cp conf.fortify.jsFiles -exclude conf.fortify.excludeGeneratedJSFiles'
     // sh './sourceanalyzer -b conf.fortify.buildID -scan -f conf.fortify.scanFilePath'
   }
 }
