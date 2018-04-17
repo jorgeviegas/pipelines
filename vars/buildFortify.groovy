2b0b5ddebeabdce88f00eb9df692cc2288bae07b
Cassiano Jaeger Stradolini <cassianojaeger@gmail.com>
2018-04-17 15:23:17 -0300

    test

--- vars/buildFortify.groovy
+++ vars/buildFortify.groovy
@@ -3,12 +3,12 @@
 def call(Map conf = [:]) {
 
   dir(conf.fortify_binaries){
-    echo "./sourceanalyzer -b ${conf.fortify_buildID} -cp ${workspace}\/${conf.fortify_javaFiles} -exclude \"${workspace}${conf.fortify_excludeGenSrc}\""
-    echo "./sourceanalyzer -b ${conf.fortify_buildID} -cp ${workspace}\/${conf.fortify_javaFiles} -exclude \"${workspace}${conf.fortify_excludeGenSrc}\""
+    echo "./sourceanalyzer -b ${conf.fortify_buildID} -cp ${workspace}/${conf.fortify_javaFiles} -exclude \"${workspace}${conf.fortify_excludeGenSrc}\""
+    echo "./sourceanalyzer -b ${conf.fortify_buildID} -cp ${workspace}/${conf.fortify_javaFiles} -exclude \"${workspace}${conf.fortify_excludeGenSrc}\""
 
-    sh "./sourceanalyzer -b ${conf.fortify_buildID} -cp ${workspace}\/${conf.fortify_javaFiles} -exclude \"${workspace}\/${conf.fortify_excludeTest}:${workspace}\/${conf.fortify_excludeGenSrc}\""
-    sh "./sourceanalyzer -b ${conf.fortify_buildID} -cp ${workspace}\/${conf.fortify_jsFiles} -exclude \"${workspace}\/${conf.fortify_exclude_UI}:${workspace}\/${conf.fortify_exclude_UISource}\""
-    sh "./sourceanalyzer -b ${conf.fortify_buildID} -scan -f ${workspace}\/${conf.fortify_scanFilePath}"
+    sh "./sourceanalyzer -b ${conf.fortify_buildID} -cp ${workspace}/${conf.fortify_javaFiles} -exclude \"${workspace}/${conf.fortify_excludeTest}:${workspace}/${conf.fortify_excludeGenSrc}\""
+    sh "./sourceanalyzer -b ${conf.fortify_buildID} -cp ${workspace}/${conf.fortify_jsFiles} -exclude \"${workspace}/${conf.fortify_exclude_UI}:${workspace}/${conf.fortify_exclude_UISource}\""
+    sh "./sourceanalyzer -b ${conf.fortify_buildID} -scan -f ${workspace}/${conf.fortify_scanFilePath}"
   }
 }
 //
