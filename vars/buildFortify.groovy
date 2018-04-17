fc6a1d329b24cba42ba7cf66642355fc7250b1a2
Cassiano Jaeger Stradolini <cassianojaeger@gmail.com>
2018-04-17 15:54:39 -0300

    test

--- vars/buildFortify.groovy
+++ vars/buildFortify.groovy
@@ -2,6 +2,7 @@
 
 def call(Map conf = [:]) {
   dir(conf.fortify_binaries){
+    sh "./sourceanalyzer -b ${conf.fortify_buildID} -clean"
     sh "./sourceanalyzer -b ${conf.fortify_buildID} -classpath ${workspace}/${conf.fortify_javaFiles} -exclude ${workspace}/${conf.fortify_excludeTest}:${workspace}/${conf.fortify_excludeGenSrc} ${workspace}/${conf.fortify_javaFiles} "
     sh "./sourceanalyzer -b ${conf.fortify_buildID} -classpath ${workspace}/${conf.fortify_jsFiles} -exclude ${workspace}/${conf.fortify_exclude_UI}:${workspace}/${conf.fortify_exclude_UISource} ${workspace}/${conf.fortify_jsFiles} "
     sh "./sourceanalyzer -b ${conf.fortify_buildID} -scan -f ${workspace}/${conf.fortify_scanFilePath}"
