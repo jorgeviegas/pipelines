aed54973db829dd75259bf259c4085804da8a71d
Cassiano Jaeger Stradolini <cassianojaeger@gmail.com>
2018-04-17 13:57:22 -0300

    new test

--- vars/buildFortify.groovy
+++ vars/buildFortify.groovy
@@ -4,12 +4,10 @@ def call(Map conf = [:]) {
 
   dir(conf.fortify_binaries){
 
-    echo "whoami"
+    echo "./sourceanalyzer -b ${conf.fortify_buildID} -cp ${workspace}${conf.fortify_javaFiles} -exclude \"${workspace}${conf.fortify_excludeGenSrc}\""
 
-    echo "./sourceanalyzer -b ${conf.fortify.buildID} -cp ${workspace}${conf.fortify_javaFiles} -exclude \"${workspace}${conf.fortify_excludeGenSrc}\""
-
-    sh "./sourceanalyzer -b ${conf.fortify.buildID} -cp ${workspace}${conf.fortify.javaFiles} -exclude \"${workspace}${conf.fortify.excludeTest}:${conf.fortify_excludeGenSrc}\""
-    sh "./sourceanalyzer -b ${conf.fortify.buildID} -cp ${workspace}${conf.fortify.jsFiles} -exclude \"${workspace}${conf.fortify_exclude_UI}:${conf.fortify_exclude_UISource}\""
-    sh "./sourceanalyzer -b ${conf.fortify.buildID} -scan -f ${conf.fortify.scanFilePath}"
+    sh "./sourceanalyzer -b ${conf.fortify_buildID} -cp ${workspace}${conf.fortify_javaFiles} -exclude \"${workspace}${conf.fortify_excludeTest}:${conf.fortify_excludeGenSrc}\""
+    sh "./sourceanalyzer -b ${conf.fortify_buildID} -cp ${workspace}${conf.fortify_jsFiles} -exclude \"${workspace}${conf.fortify_exclude_UI}:${conf.fortify_exclude_UISource}\""
+    sh "./sourceanalyzer -b ${conf.fortify_buildID} -scan -f ${conf.fortify_scanFilePath}"
   }
 }
